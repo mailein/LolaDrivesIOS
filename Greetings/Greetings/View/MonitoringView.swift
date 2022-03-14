0f724474bbda6229243405eb1c37b3f23aac57f8
Mei Chen <s8mechen@stud.uni-saarland.de>
2022-03-14 22:48:16 +0100

    display updated value for commands in selected profile

--- Greetings/Greetings/View/MonitoringView.swift
+++ Greetings/Greetings/View/MonitoringView.swift
@@ -12,10 +12,68 @@ struct MonitoringView: View {
                 Text(command.name)
                 Spacer()
                 VStack(alignment: .trailing){
-                    Text(obd.mySpeed)
-//                    Text(command.unit)
-//                        .italic()
-//                        .fontWeight(.ultraLight)
+                    switch command.pid {
+                    case "05":
+                        Text(obd.myCoolantTemp)
+                    case "0C":
+                        Text(obd.myRPM)
+                    case "0D":
+                        Text(obd.mySpeed)
+                    case "0F":
+                        Text(obd.myIntakeTemp)
+                    case "10":
+                        Text(obd.myMAFRate)
+                    case "66":
+                        Text(obd.myMAFRateSensor)
+                    case "24":
+                        Text(obd.myOxygenSensor1)
+                    case "2C":
+                        Text(obd.myCommandedEgr)
+                    case "2F":
+                        Text(obd.myFuelTankLevelInput)
+                    case "3C":
+                        Text(obd.myCatalystTemp11)
+                    case "3E":
+                        Text(obd.myCatalystTemp12)
+                    case "3D":
+                        Text(obd.myCatalystTemp21)
+                    case "3F":
+                        Text(obd.myCatalystTemp22)
+                    case "44":
+                        Text(obd.myAirFuelEqvRatio)
+                    case "46":
+                        Text(obd.myAmbientAirTemp)
+                    case "4F":
+                        Text("\(obd.myMaxValueFuelAirEqvRatio) | \(obd.myMaxValueOxygenSensorVoltage) | \(obd.myMaxValueOxygenSensorCurrent) | \(obd.myMaxValueIntakeMAP)")
+                    case "50":
+                        Text(obd.myMaxAirFlowRate)
+                    case "51":
+                        Text(obd.myFuelType)
+                    case "5C":
+                        Text(obd.myEngineOilTemp)
+                    case "68":
+                        Text(obd.myIntakeAirTempSensor)
+                    case "83":
+                        Text(obd.myNox)
+                    case "A1":
+                        Text(obd.myNoxCorrected)
+                    case "A7":
+                        Text(obd.myNoxAlternative)
+                    case "A8":
+                        Text(obd.myNoxCorrectedAlternative)
+                    case "86":
+                        Text(obd.myPmSensor)
+                    case "5E":
+                        Text(obd.myEngineFuelRate)
+                    case "9D":
+                        Text(obd.myEngineFuelRateMulti)
+                    case "9E":
+                        Text(obd.myEngineExhaustFlowRate)
+                    case "2D":
+                        Text(obd.myEgrError)
+                    default:
+                        Text("")
+                    }
                 }
             }
         }
