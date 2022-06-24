825acd48c09be0685bfd16dd23101f44711aff4d
Mei Chen <s8mechen@stud.uni-saarland.de>
2022-06-24 14:35:43 +0200

    rde result help msg

--- Greetings/Greetings/View/RdeLogView.swift
+++ Greetings/Greetings/View/RdeLogView.swift
@@ -53,124 +53,35 @@ struct RdeResultView: View{
     
     var body: some View{
         VStack(alignment: .leading){
-            RdeResultLine(name: "Valid RDE Trip:", imageName: getValidRdeTrip(), helpMsg: "valid rde trip help msg test test")
-            RdeResultLine(name: "Total Duration:", durationText: DurationText(durationInSeconds: Int64(getTotalDuration())))
-            RdeResultLine(name: "Total Distance:", distanceText: DistanceText(distanceInMeters: getTotalDistance()))
-            RdeResultLine(name: "NOₓ Emissions:", value: getNoxPerKilometer(), unit: "mg/km")
+            RdeResultLine(name: "Valid RDE Trip", imageName: getValidRdeTrip(), helpMsg: .validRdeTrip)
+            RdeResultLine(name: "Total Duration", durationText: DurationText(durationInSeconds: Int64(getTotalDuration())), helpMsg: .totalDuration)
+            RdeResultLine(name: "Total Distance", distanceText: DistanceText(distanceInMeters: getTotalDistance()))
+            RdeResultLine(name: "NOₓ Emissions", value: getNoxPerKilometer(), unit: "mg/km", helpMsg: .nox)
             
-            RdeResultSection(text: "Urban",
-                             t: outputs["d_u"] ?? 0,
-                             d: outputs["t_u"] ?? 0,
+            RdeResultSection(category: .URBAN,
+                             t: outputs["t_u"] ?? 0,
+                             d: outputs["d_u"] ?? 0,
                              avg: outputs["u_avg_v"] ?? 0,
                              pct: outputs["u_va_pct"] ?? 0,
-                             rpa: outputs["u_rpa"] ?? 0)
+                             rpa: outputs["u_rpa"] ?? 0,
+                             totalDistance: outputs["d"] ?? 0)
 
-            RdeResultSection(text: "Rural",
-                             t: outputs["d_r"] ?? 0,
-                             d: outputs["t_r"] ?? 0,
+            RdeResultSection(category: .RURAL,
+                             t: outputs["t_r"] ?? 0,
+                             d: outputs["d_r"] ?? 0,
                              avg: outputs["r_avg_v"] ?? 0,
                              pct: outputs["r_va_pct"] ?? 0,
-                             rpa: outputs["r_rpa"] ?? 0)
+                             rpa: outputs["r_rpa"] ?? 0,
+                             totalDistance: outputs["d"] ?? 0)
 
-            RdeResultSection(text: "Motorway",
-                             t: outputs["d_m"] ?? 0,
-                             d: outputs["t_m"] ?? 0,
+            RdeResultSection(category: .MOTORWAY,
+                             t: outputs["t_m"] ?? 0,
+                             d: outputs["d_m"] ?? 0,
                              avg: outputs["m_avg_v"] ?? 0,
                              pct: outputs["m_va_pct"] ?? 0,
-                             rpa: outputs["m_rpa"] ?? 0)
+                             rpa: outputs["m_rpa"] ?? 0,
+                             totalDistance: outputs["d"] ?? 0)
             
-        }
-    }
-    
-    struct RdeResultSection: View {
-        let text: String
-        let t: Double
-        let d: Double
-        let avg: Double
-        let pct: Double
-        let rpa: Double
-        
-        var body: some View {
-            VStack(alignment: .leading){
-                Divider()
-                Text(text).bold()
-                RdeResultLine(name: "Duration", durationText: DurationText(durationInSeconds: Int64(t)))
-                RdeResultLine(name: "Distance", distanceText: DistanceText(distanceInMeters: d))
-                RdeResultLine(name: "Average Speed", value: avg, unit: "km/h")
-                RdeResultLine(name: "95Percentile(va)", value: pct, unit: "m^2/s^3")
-                RdeResultLine(name: "RPA", value: rpa, unit: "m/s^2")
-            }
-        }
-    }
-    
-    struct RdeResultLine: View {
-        private let name: String
-        private var value: Double? = nil
-        private var unit: String? = nil
-        private var imageName: String? = nil
-        private var duration: DurationText? = nil
-        private var distance: DistanceText? = nil
-        private var helpMsg: String?
-        private let width: CGFloat = 150
-        
-        @State private var showPopover = false
-        
-        init(name: String, value: Double, unit: String, helpMsg: String? = nil) {
-            self.name = name
-            self.value = value
-            self.unit = unit
-            self.helpMsg = helpMsg
-        }
-        
-        init(name: String, imageName: String, helpMsg: String? = nil) {
-            self.name = name
-            self.imageName = imageName
-            self.helpMsg = helpMsg
-        }
-        
-        init(name: String, durationText: DurationText, helpMsg: String? = nil) {
-            self.name = name
-            self.duration = durationText
-            self.helpMsg = helpMsg
-        }
-        
-        init(name: String, distanceText: DistanceText, helpMsg: String? = nil) {
-            self.name = name
-            self.distance = distanceText
-            self.helpMsg = helpMsg
-        }
-        
-        var body: some View {
-            HStack(alignment: .bottom){
-                Text(name)
-                    .frame(width: width, alignment: .bottomLeading)
-                if value != nil && unit != nil {
-                    Text("\(String(format: "%.2f", value!)) \(unit!)")
-                }
-                if imageName != nil {
-                    let color = imageName! == "xmark" ? Color.red : Color.green
-                    Image(systemName: imageName!)
-                        .foregroundColor(color)
-                }
-                if duration != nil {
-                    duration!
-                }
-                if distance != nil {
-                    distance!
-                }
-                Spacer()
-                if helpMsg != nil {
-                    Button(action: {
-                        showPopover = true
-                    }, label: {
-                        Image(systemName: "questionmark.circle")
-                    })
-                    .frame(alignment: .trailing)
-                    .popover(isPresented: $showPopover, content: {
-                        Text(helpMsg!)
-                    })
-                }
-            }
         }
     }
     
@@ -198,8 +109,265 @@ struct RdeResultView: View{
         let nox: Double = outputs["nox_per_kilometer"] ?? 0
         return nox * 1000
     }
+    
+    enum HelpMsg: String {
+        case validRdeTrip = "Valid RDE Trip"
+        case totalDuration = "Total Duration"
+        case nox = "NOₓ Emissions"
+        
+        case urbanDistance = "Urban - Distance"
+        case urbanPct = "Urban - Dynamics High"
+        case urbanRpa = "Urban - Dynamics Low"
+        
+        case ruralDistance = "Rural - Distance"
+        case ruralPct = "Rural - Dynamics High"
+        case ruralRpa = "Rural - Dynamics Low"
+        
+        case motorwayDistance = "Motorway - Distance"
+        case motorwayPct = "Motorway - Dynamics High"
+        case motorwayRpa = "Motorway - Dynamics Low"
+    }
+    
+    struct RdeResultSection: View {
+        let category: Category
+        let t: Double
+        let d: Double
+        let avg: Double
+        let pct: Double
+        let rpa: Double
+        let helpDistance: HelpMsg
+        let helpDynamicsHigh: HelpMsg
+        let helpDynamicsLow: HelpMsg
+        let totalDistance: Double
+        
+        init(category: Category, t: Double, d: Double, avg: Double, pct: Double, rpa: Double, totalDistance: Double) {
+            self.category = category
+            self.t = t
+            self.d = d
+            self.avg = avg
+            self.pct = pct
+            self.rpa = rpa
+            self.totalDistance = totalDistance
+            switch category {
+            case .URBAN:
+                self.helpDistance = .urbanDistance
+                self.helpDynamicsHigh = .urbanPct
+                self.helpDynamicsLow = .urbanRpa
+            case .RURAL:
+                self.helpDistance = .ruralDistance
+                self.helpDynamicsHigh = .ruralPct
+                self.helpDynamicsLow = .ruralRpa
+            case .MOTORWAY:
+                self.helpDistance = .motorwayDistance
+                self.helpDynamicsHigh = .motorwayPct
+                self.helpDynamicsLow = .motorwayRpa
+            }
+        }
+        
+        var body: some View {
+            VStack(alignment: .leading){
+                Divider()
+                Text(category.rawValue).bold()
+                RdeResultLine(name: "Duration", durationText: DurationText(durationInSeconds: Int64(t)))
+                RdeResultLine(name: "Distance", distanceText: DistanceText(distanceInMeters: d), helpMsg: self.helpDistance, totalDistance: self.totalDistance)
+                RdeResultLine(name: "Average Speed", value: avg, unit: "km/h")
+                RdeResultLine(name: "Dynamics High", value: pct, unit: "m²/s³", helpMsg: self.helpDynamicsHigh, avg_speed: avg)
+                RdeResultLine(name: "Dynamics Low", value: rpa, unit: "m/s²", helpMsg: self.helpDynamicsLow, avg_speed: avg)
+            }
+        }
+    }
+    
+    struct RdeResultLine: View {
+        private let name: String
+        private var value: Double? = nil
+        private var unit: String? = nil
+        private var imageName: String? = nil
+        private var duration: DurationText? = nil
+        private var distance: DistanceText? = nil
+        private var helpMsg: HelpMsg?
+        private var totalDistance: Double?
+        private var avg_speed: Double?
+        
+        private let width: CGFloat = 150
+        @State private var showPopover = false
+        
+        init(name: String, value: Double, unit: String, helpMsg: HelpMsg? = nil, avg_speed: Double? = nil) {
+            self.name = name
+            self.value = value
+            self.unit = unit
+            self.helpMsg = helpMsg
+            self.avg_speed = avg_speed
+        }
+        
+        init(name: String, imageName: String, helpMsg: HelpMsg? = nil) {
+            self.name = name
+            self.imageName = imageName
+            self.helpMsg = helpMsg
+        }
+        
+        init(name: String, durationText: DurationText, helpMsg: HelpMsg? = nil) {
+            self.name = name
+            self.duration = durationText
+            self.helpMsg = helpMsg
+        }
+        
+        init(name: String, distanceText: DistanceText, helpMsg: HelpMsg? = nil, totalDistance: Double? = nil) {
+            self.name = name
+            self.distance = distanceText
+            self.helpMsg = helpMsg
+            self.totalDistance = totalDistance
+        }
+        
+        var body: some View {
+            HStack(alignment: .center){
+                Text("\(name):")
+                    .frame(width: width, alignment: .bottomLeading)
+                if value != nil && unit != nil {
+                    Text("\(String(format: "%.2f", value!)) \(unit!)")
+                }
+                if imageName != nil {
+                    let color = imageName! == "xmark" ? Color.red : Color.green
+                    Image(systemName: imageName!)
+                        .foregroundColor(color)
+                }
+                if duration != nil {
+                    duration!
+                }
+                if distance != nil {
+                    distance!
+                }
+                Spacer()
+                if helpMsg != nil {
+                    Button(action: {
+                        showPopover = true
+                    }, label: {
+                        Image(systemName: "questionmark.circle")
+                    })
+                    .frame(alignment: .trailing)
+                    .popover(isPresented: $showPopover, content: {
+                        helpMsgView(help: helpMsg!, totalDistance: self.totalDistance, avg_speed: avg_speed)
+                    })
+                }
+            }
+        }
+        
+        struct helpMsgView: View {
+            let help: HelpMsg
+            var totalDistance: Double? = -1
+            var avg_speed: Double? = -1
+            let headlineColor = Color.cyan
+            
+            init(help: HelpMsg, totalDistance: Double? = nil, avg_speed: Double? = nil){
+                self.help = help
+                switch help{
+                case .urbanDistance, .ruralDistance, .motorwayDistance:
+                    self.totalDistance = totalDistance
+                    self.avg_speed = nil
+                case .urbanPct, .urbanRpa, .ruralPct, .ruralRpa, .motorwayPct, .motorwayRpa:
+                    self.totalDistance = nil
+                    self.avg_speed = avg_speed
+                default:
+                    self.totalDistance = nil
+                    self.avg_speed = nil
+                }
+            }
+            
+            var body: some View {
+                VStack(alignment: .leading, spacing: 20){
+                    Text(help.rawValue)
+                    .font(.title)
+                    .bold()
+                    .foregroundColor(headlineColor)
+                    
+                    switch help {
+                    case .validRdeTrip:
+                        Text("The EU defines several constraints for test drives to be considered as a valid RDE test.")
+                        Text("The most important conditions are listed on this result summary page.")
+                        Text("If you want to learn more about a constraint, or to get tips on how to keep a value within the acceptable bounds, you can tap on the \(Image(systemName: "questionmark.circle")) next to the respective value.")
+                    case .totalDuration:
+                        Text("The total duration of a valid RDE-Drive must be between **90 and 120 minutes**")
+                        Text("Too short or too long drives can not be valid.")
+                    case .nox:
+                        Text("The NOₓ emissions value indicates how much nitrogen oxides the tested vehicle emitted per kilometer during the RDE drive.")
+                        Text("These values differ depending on the **year of type approval** and the **year of first admission** of the vehicle.")
+                        Text("Diesel powered vehicle types approved between September 2017 and December 2019, or vehicles firstly admitted between September 2019 and December 2020, must emit at most **168 mg/km** of nitric oxides. Diesel cars approved or admitted later, must conform to a **120 mg/km** threshold.")
+                    case .urbanDistance, .ruralDistance, .motorwayDistance:
+                        helpMsgDistanceView(totalDistance: totalDistance!)
+                    case .urbanPct, .ruralPct, .motorwayPct:
+                        helpMsgPctView(avg_speed: avg_speed!)
+                    case .urbanRpa, .ruralRpa, .motorwayRpa:
+                        helpMsgRpaView(avg_speed: avg_speed!)
+                    }
+                }
+                .padding()
+            }
+            
+            struct helpMsgDistanceView: View {
+                let totalDistance: Double
+                
+                var body: some View {
+                    Text("The Urban Distance indicates how much distance was covered during the test in the **urban segment**, i.e. in towns and cities.")
+                    Text("This distance must be greater than **16 km** and must also constitute **29% to 44% of the total distance** of the RDE-Drive.")
+                    if totalDistance * 0.44 / 1000.0 < 16.0 {
+                        Text("So the distance in this drive must be greater than **16 km**")
+                    } else if totalDistance * 0.29 < 16_000 {
+                        Text("So the distance in this drive must be between **16 km** and **\(totalDistance * 0.44 / 1000.0) km**")
+                    } else {
+                        Text("So the distance in this drive must be between **\(totalDistance * 0.29 / 1000.0) km** and **\(totalDistance * 0.44 / 1000.0) km**")
+                    }
+                }
+            }
+            
+            struct helpMsgPctView: View {
+                let avg_speed: Double
+                
+                var body: some View {
+                    Text("In order to exclude driving that could be regarded as **too aggressive**, the high dynamic boundary condition sets an upper limit for the acceleration behavior.")
+                    Text("It is checked whether the 95th percentile of the product of acceleration and speed is below a limit set by the EU.")
+                    Text("This limit depends on the average speed of the corresponding segment.")
+                    
+                    Text("In this drive and segment, the upper limit is **\(calculateHighDynamics(avg_speed: avg_speed)) m²/s³**")
+                }
+                
+                func calculateHighDynamics(avg_speed: Double) -> String{
+                    let value: Double
+                    if avg_speed < 74.6 {
+                        if avg_speed == 0.0 {
+                            value = 0.0
+                        } else {
+                            value = 0.136 * avg_speed + 14.44
+                        }
+                    } else {
+                        value = 0.0742 * avg_speed + 18.966
+                    }
+                    return String(format: "%.2f", value)
+                }
+            }
+            
+            struct helpMsgRpaView: View {
+                let avg_speed: Double
+                
+                var body: some View {
+                    Text("In order to exclude driving that could be regarded as **too smooth**, the low dynamic boundary condition sets a lower limit for the **relative positive acceleration (RPA)**.")
+                    Text("It is checked whether the ratio of the distance accelerated to the total distance of a segment exceeds a limit set by the EU.")
+                    Text("This limit depends on the average speed of the corresponding segment.")
+                    Text("In this drive and segment, the lower limit is **\(calculateLowDynamics(avg_speed: avg_speed)) m/s²**")
+                }
+                
+                func calculateLowDynamics(avg_speed: Double) -> String {
+                    let value: Double
+                    if avg_speed < 94.05 {
+                        if avg_speed == 0.0 {
+                            value = 0.0
+                        } else {
+                            value = -0.0016 * avg_speed + 0.1755
+                        }
+                    } else {
+                        value = 0.025
+                    }
+                    return String(format: "%.2f", value)
+                }
+            }
+        }
+    }
 }
-
-
-
-
